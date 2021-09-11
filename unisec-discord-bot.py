68680717a08f7a2a691b7c62c1f5315c8fc4d5ab
Chanotai Krajeam <boom9387@hotmail.com>
2021-09-11 19:18:36 +0700

    test unisec bot

---
+++ unisec-discord-bot.py
@@ -0,0 +1,47 @@
+from re import match
+import discord
+import random
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+client = discord.Client()
+
+async def createTeam(guild, author):
+    allTeams = [x for x in guild.roles if x.name.find("team") != -1]
+    role = await guild.create_role(name=f"team{str(len(allTeams) + 1).zfill(2)}", colour=discord.Colour(0xF6BE00))
+    await author.add_roles(role)
+    return role
+
+@client.event
+async def on_ready() :
+    print(f"Logged in as {client.user}")
+
+@client.event
+async def on_message(ctx):
+    message = ctx.content
+    guild = ctx.guild
+    author = ctx.author
+    
+    if message.startswith('!team'):
+        try:
+            command = message.split(" ")[1]
+        except:
+            command = "help"
+            
+        if command == "add":
+            if len(message.split(" ")) >= 2 :
+                userTeam = [x for x in author.roles if x.name.find("team") != -1]
+                if not len(userTeam) :
+                    newRole = await createTeam(guild, author)
+
+                for mention in ctx.mentions:
+                    await discord.Member.add_roles(mention, userTeam[0] if len(userTeam) else newRole)
+            else :
+                await ctx.channel.send("You have entered wrong command. please try, !team add <user>")
+        elif command == "help":
+            print('help')
+        else:
+            print('unknown')
+
+client.run(os.getenv('UNISEC_TOKEN'))
